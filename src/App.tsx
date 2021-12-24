7a1e1f69c1d9351a31872d7746103533f0672949
Pierre Beaujeu <pierre.beaujeu@gmail.com>
2021-12-24 15:23:22 +0100

    add advices

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import { useState } from 'react';
-import { CountDownComponent } from './component/CountDown';
-import { Wrapper, ButtonWrapper } from './App.styled';
+import React from "react";
+import { useState } from "react";
+import { CountDown } from "./component/CountDown";
+import { Wrapper, ButtonWrapper } from "./App.styled";
 
 function App() {
   // Additonal code for testing 'nb' prop change
-  const [ number, setNumber] = useState(0);
+  const [number, setNumber] = useState(0);
   const changeNumber = () => {
-    const randomNumber = Math.round((Math.random() * 10) + 1);
+    const randomNumber = Math.round(Math.random() * 10 + 1);
     setNumber(randomNumber);
   };
-  
+
   const onDone = () => {
-    console.log('onDone function...');
-  }
+    console.log("onDone function...");
+  };
   return (
     <Wrapper className="App">
       <h1>Countdown component</h1>
-      <CountDownComponent nb={number} onDone={onDone}/>
+      <CountDown nb={number} onDone={onDone} />
       <ButtonWrapper onClick={changeNumber}>Change 'nb' prop</ButtonWrapper>
-      <div className='infos'>When counter reaches 0, "onDone function" appears in the console and counter starts again</div>
+      <div className="infos">
+        When counter reaches 0, "onDone function" appears in the console and
+        counter starts again
+      </div>
     </Wrapper>
   );
 }
